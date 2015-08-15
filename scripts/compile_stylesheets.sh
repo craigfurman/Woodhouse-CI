de3f8d5706f4a674689f36472dda24e0367a89e4
Craig Furman <craig.furman89@gmail.com>
2015-08-15 10:56:24 +0100

    Basic bootstrap styling
    Use Droid Sans Mono (Apache Licensed TTF) for build output

--- scripts/compile_stylesheets.sh
+++ scripts/compile_stylesheets.sh
@@ -2,4 +2,10 @@
 
 project=$(dirname $0)/..
 
-bundle exec compass compile --sass-dir $project/web/scss --css-dir $project/web/assets/stylesheets 
+# Need to supply relative path for --fonts-dir
+pushd $project
+bundle exec compass compile \
+  --sass-dir $project/web/scss \
+  --css-dir $project/web/assets/stylesheets \
+  --fonts-dir web/assets/fonts
+popd
