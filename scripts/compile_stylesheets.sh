6b6a4cfca995bcc9f760cfce4ff3d935a8b13076
Craig Furman <craig.furman89@gmail.com>
2015-08-31 11:59:40 +0100

    run sass watch through script

--- scripts/compile_stylesheets.sh
+++ scripts/compile_stylesheets.sh
@@ -1,10 +1,17 @@
 #!/bin/bash -e
 
+sass_cmd="compile"
+if [ -n "$1" ]
+then
+  sass_cmd=$1
+  shift
+fi
+
 project=$(dirname $0)/..
 
 # Need to supply relative path for --fonts-dir
 pushd $project
-bundle exec compass compile \
+bundle exec compass $sass_cmd \
   --sass-dir $project/web/scss \
   --css-dir $project/web/assets/stylesheets \
   --fonts-dir web/assets/fonts
